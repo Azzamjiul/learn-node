57b6abb276d11a23a706328154b700a3efb69467
Azzam Jihad Ulhaq <azzam.jihad@mekari.com>
2022-04-01 17:39:36 +0700

    timer

---
+++ timer-promise.mjs
@@ -0,0 +1,10 @@
+import timers from "timers/promises";
+
+// console.info(`${new Date()}`);
+// const val = await timers.setTimeout(5000, "Ini value setelah resolve promise");
+// console.info(`${new Date()}`);
+// console.info(val);
+
+for await (const startTime of timers.setInterval(1000, `${new Date()}`)) {
+    console.info(`Start Timer at ${startTime}`)
+}
